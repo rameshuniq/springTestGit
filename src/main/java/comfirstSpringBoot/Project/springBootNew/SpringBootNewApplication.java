01d07c582cfab02f64df43e4f143284efd0bab3c
ramesh.kumar844@gmail.com <ramesh.kumar844@gmail.comgit config --global user.email ramesh.kumar844@gmail.com>
2022-11-04 16:35:24 +0530

    firstcommit

---
+++ src/main/java/comfirstSpringBoot/Project/springBootNew/SpringBootNewApplication.java
@@ -0,0 +1,13 @@
+package comfirstSpringBoot.Project.springBootNew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/comfirstSpringBoot/Project/springBootNew/SpringBootNewApplicationTests.java
@@ -0,0 +1,13 @@
+package comfirstSpringBoot.Project.springBootNew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
